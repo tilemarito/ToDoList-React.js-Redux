0d2915f421143cd00ce546be2a853794e9d0ff16
tilemarito <tilemarito123@gmail.com>
2023-05-26 15:29:12 +0800

    add redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import todoReducer from "./reducers/todoReducer";
+import todoReducer from "./reducers/TodoReducer";
 
 const store = createStore(todoReducer)
 
